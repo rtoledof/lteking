47ab6e02a4db6c3f2c06878e3e856de6af56f637
Rolando Toledo Fernandez <rtoledofernandez@globalprimex.com>
2023-12-19 02:21:04 +0100

    add rate service and tests

--- pkg/cubawheeler/rate.go
+++ pkg/cubawheeler/rate.go
@@ -1,6 +1,11 @@
 package cubawheeler
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"cubawheeler.io/pkg/errors"
+)
 
 type Rate struct {
 	ID                string `json:"id" bson:"_id"`
@@ -8,14 +13,31 @@ type Rate struct {
 	BasePrice         int    `json:"base_price" bson:"base_price"`
 	PricePerMin       int    `json:"price_per_min" bson:"price_per_min"`
 	PricePerKm        int    `json:"price_per_km" bson:"price_per_km"`
-	PricePerPassenger *int   `json:"price_per_passenger,omitempty" bson:"price_per_passenger,omitempty"`
+	PricePerPassenger int    `json:"price_per_passenger,omitempty" bson:"price_per_passenger,omitempty"`
 	PricePerBaggage   int    `json:"price_per_baggage" bson:"price_per_baggage"`
-	StartTime         *int   `json:"start_time,omitempty" bson:"start_time,omitempty"`
-	EndTime           *int   `json:"end_time,omitempty" bson:"end_time,omitempty"`
-	StartDate         *int   `json:"start_date,omitempty" bson:"start_date,omitempty"`
-	EndDate           *int   `json:"end_date,omitempty" bson:"end_date,omitempty"`
-	MinKm             *int   `json:"min_km,omitempty" bson:"min_km,omitempty"`
-	MaxKm             *int   `json:"max_km,omitempty" bson:"max_km,omitempty"`
+	StartTime         int    `json:"start_time,omitempty" bson:"start_time,omitempty"`
+	EndTime           int    `json:"end_time,omitempty" bson:"end_time,omitempty"`
+	StartDate         int    `json:"start_date,omitempty" bson:"start_date,omitempty"`
+	EndDate           int    `json:"end_date,omitempty" bson:"end_date,omitempty"`
+	MinKm             int    `json:"min_km,omitempty" bson:"min_km,omitempty"`
+	MaxKm             int    `json:"max_km,omitempty" bson:"max_km,omitempty"`
+}
+
+func (r *Rate) Validate() error {
+	if r.Code == "" {
+		return fmt.Errorf("code is required: %w", errors.ErrInvalidInput)
+	}
+	if r.BasePrice <= 0 {
+		return fmt.Errorf("base price is required: %w", errors.ErrInvalidInput)
+	}
+	if r.PricePerMin <= 0 {
+		return fmt.Errorf("price per min is required: %w", errors.ErrInvalidInput)
+	}
+	if r.PricePerKm <= 0 {
+		return fmt.Errorf("price per km is required: %w", errors.ErrInvalidInput)
+	}
+
+	return nil
 }
 
 type RateRequest struct {
@@ -35,12 +57,21 @@ type RateRequest struct {
 }
 
 type RateFilter struct {
-	Ids []string
+	Ids       []string
+	Token     string
+	Limit     int
+	Code      []string
+	MinPrice  int
+	MaxPrice  int
+	StartDate int
+	EndDate   int
+	StartTime int
+	EndTime   int
 }
 
 type RateService interface {
 	Create(context.Context, RateRequest) (*Rate, error)
 	Update(context.Context, *RateRequest) (*Rate, error)
-	FindID(context.Context, string) (*Rate, error)
-	FindAll(context.Context, *RateRequest) ([]Rate, string, error)
+	FindByID(context.Context, string) (*Rate, error)
+	FindAll(context.Context, RateFilter) ([]*Rate, string, error)
 }
--- pkg/mongo/rate.go
+++ pkg/mongo/rate.go
@@ -2,71 +2,194 @@ package mongo
 
 import (
 	"context"
-	"errors"
-
-	"go.mongodb.org/mongo-driver/mongo"
+	"fmt"
 
 	"cubawheeler.io/pkg/cubawheeler"
+	"cubawheeler.io/pkg/errors"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var _ cubawheeler.RateService = &RateService{}
 
+var RatesCollection Collections = "rates"
+
 type RateService struct {
-	db         *DB
-	collection *mongo.Collection
+	db *DB
 }
 
 func NewRateService(db *DB) *RateService {
 	return &RateService{
-		db:         db,
-		collection: db.client.Database(database).Collection("rates"),
+		db: db,
 	}
 }
 
 func (s *RateService) Create(ctx context.Context, request cubawheeler.RateRequest) (*cubawheeler.Rate, error) {
 	usr := cubawheeler.UserFromContext(ctx)
 	if usr == nil {
-		return nil, errors.New("invalid token provided")
+		return nil, errors.ErrAccessDenied
 	}
 	if usr.Role != cubawheeler.RoleAdmin {
-		return nil, errors.New("access denied")
+		return nil, errors.ErrOrderAccepted
 	}
-	rate := &cubawheeler.Rate{
-		ID:                cubawheeler.NewID().String(),
-		Code:              request.Code,
-		BasePrice:         request.BasePrice,
-		PricePerMin:       *request.PricePerMin,
-		PricePerKm:        *request.PricePerKm,
-		PricePerPassenger: request.PricePerPassenger,
-		PricePerBaggage:   *request.PricePerBaggage,
-		StartDate:         request.StartDate,
-		EndDate:           request.EndDate,
-		StartTime:         request.StartTime,
-		EndTime:           request.EndTime,
-		MinKm:             request.MinKm,
-		MaxKm:             request.MaxKm,
+	var rate cubawheeler.Rate
+	assembleRate(&rate, request)
+	if err := rate.Validate(); err != nil {
+		return nil, err
 	}
-	if _, err := s.collection.InsertOne(ctx, rate); err != nil {
+	if err := insertRate(ctx, s.db, &rate); err != nil {
+		return nil, err
+	}
+
+	return &rate, nil
+}
+
+func (s *RateService) Update(ctx context.Context, request *cubawheeler.RateRequest) (*cubawheeler.Rate, error) {
+	usr := cubawheeler.UserFromContext(ctx)
+	if usr == nil {
+		return nil, errors.ErrAccessDenied
+	}
+	if usr.Role != cubawheeler.RoleAdmin {
+		return nil, errors.ErrOrderAccepted
+	}
+	rate, err := findRateByID(ctx, s.db, request.ID)
+	if err != nil {
+		return nil, err
+	}
+	assembleRate(rate, *request)
+	if err := rate.Validate(); err != nil {
+		return nil, err
+	}
+	if err := updateRate(ctx, s.db, rate); err != nil {
 		return nil, err
 	}
 	return rate, nil
 }
 
-func (s *RateService) Update(ctx context.Context, request *cubawheeler.RateRequest) (*cubawheeler.Rate, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *RateService) FindByID(ctx context.Context, id string) (*cubawheeler.Rate, error) {
+	return findRateByID(ctx, s.db, id)
 }
 
-func (s *RateService) FindID(ctx context.Context, id string) (*cubawheeler.Rate, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *RateService) FindAll(ctx context.Context, request cubawheeler.RateFilter) ([]*cubawheeler.Rate, string, error) {
+	return findRates(ctx, s.db, &request)
 }
 
-func (s *RateService) FindAll(ctx context.Context, request *cubawheeler.RateRequest) ([]cubawheeler.Rate, string, error) {
-	//TODO implement me
-	panic("implement me")
+func updateRate(ctx context.Context, db *DB, rate *cubawheeler.Rate) error {
+	collection := db.client.Database(database).Collection(RatesCollection.String())
+	if _, err := collection.UpdateOne(ctx, bson.D{{Key: "_id", Value: rate.ID}}, bson.D{{Key: "$set", Value: rate}}); err != nil {
+		return fmt.Errorf("unable to update rate: %v: %w", err, errors.ErrInternal)
+	}
+	return nil
 }
 
-func findRates(ctx context.Context, db *DB, filter *cubawheeler.Rate) {
+func findRates(ctx context.Context, db *DB, filter *cubawheeler.RateFilter) ([]*cubawheeler.Rate, string, error) {
+	collection := db.client.Database(database).Collection(RatesCollection.String())
+	var rates []*cubawheeler.Rate
+	var token string
+	f := bson.D{}
+	if len(filter.Ids) > 0 {
+		f = append(f, bson.E{Key: "_id", Value: bson.D{{Key: "$in", Value: filter.Ids}}})
+	}
+	if len(filter.Code) > 0 {
+		f = append(f, bson.E{Key: "code", Value: bson.D{{Key: "$in", Value: filter.Code}}})
+	}
+	if filter.MinPrice > 0 {
+		f = append(f, bson.E{Key: "base_price", Value: bson.D{{Key: "$gte", Value: filter.MinPrice}}})
+	}
+	if filter.MaxPrice > 0 {
+		f = append(f, bson.E{Key: "base_price", Value: bson.D{{Key: "$lte", Value: filter.MaxPrice}}})
+	}
+	if filter.StartDate > 0 {
+		f = append(f, bson.E{Key: "start_date", Value: bson.D{{Key: "$gte", Value: filter.StartDate}}})
+	}
+	if filter.EndDate > 0 {
+		f = append(f, bson.E{Key: "end_date", Value: bson.D{{Key: "$lte", Value: filter.EndDate}}})
+	}
+	if filter.StartTime > 0 {
+		f = append(f, bson.E{Key: "start_time", Value: bson.D{{Key: "$gte", Value: filter.StartTime}}})
+	}
+	if filter.EndTime > 0 {
+		f = append(f, bson.E{Key: "end_time", Value: bson.D{{Key: "$lte", Value: filter.EndTime}}})
+	}
+	cursor, err := collection.Find(ctx, f)
+	if err != nil {
+		return nil, "", fmt.Errorf("unable to find rates: %v: %w", err, errors.ErrInternal)
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var rate cubawheeler.Rate
+		if err := cursor.Decode(&rate); err != nil {
+			return nil, "", fmt.Errorf("unable to decode rate: %v: %w", err, errors.ErrInternal)
+		}
+		rates = append(rates, &rate)
+		if len(rates) == filter.Limit+1 && filter.Limit > 0 {
+			token = rates[filter.Limit].ID
+			rates = rates[:filter.Limit]
+		}
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, "", fmt.Errorf("unable to iterate over rates: %v: %w", err, errors.ErrInternal)
+	}
+	return rates, token, nil
+}
 
+func findRateByID(ctx context.Context, db *DB, id string) (*cubawheeler.Rate, error) {
+	rates, _, err := findRates(ctx, db, &cubawheeler.RateFilter{Ids: []string{id}, Limit: 1})
+	if err != nil {
+		return nil, err
+	}
+	if len(rates) == 0 {
+		return nil, fmt.Errorf("unable to find rate: %v: %w", id, errors.ErrNotFound)
+	}
+	return rates[0], nil
+}
+
+func insertRate(ctx context.Context, db *DB, rate *cubawheeler.Rate) error {
+	collection := db.client.Database(database).Collection(RatesCollection.String())
+	if _, err := collection.InsertOne(ctx, rate); err != nil {
+		return fmt.Errorf("unable to store the rate: %v: %w", err, errors.ErrInternal)
+	}
+	return nil
+}
+
+func assembleRate(rate *cubawheeler.Rate, req cubawheeler.RateRequest) {
+	id := req.ID
+	if id == "" {
+		id = cubawheeler.NewID().String()
+	}
+	if len(req.Code) > 0 {
+		rate.Code = req.Code
+	}
+	if req.BasePrice > 0 {
+		rate.BasePrice = req.BasePrice
+	}
+	if req.PricePerMin != nil {
+		rate.PricePerMin = *req.PricePerMin
+	}
+	if req.PricePerKm != nil {
+		rate.PricePerKm = *req.PricePerKm
+	}
+	if req.PricePerPassenger != nil {
+		rate.PricePerPassenger = *req.PricePerPassenger
+	}
+	if req.PricePerBaggage != nil {
+		rate.PricePerBaggage = *req.PricePerBaggage
+	}
+	if req.StartTime != nil {
+		rate.StartTime = *req.StartTime
+	}
+	if req.EndTime != nil {
+		rate.EndTime = *req.EndTime
+	}
+	if req.StartDate != nil {
+		rate.StartDate = *req.StartDate
+	}
+	if req.EndDate != nil {
+		rate.EndDate = *req.EndDate
+	}
+	if req.MinKm != nil {
+		rate.MinKm = *req.MinKm
+	}
+	if req.MaxKm != nil {
+		rate.MaxKm = *req.MaxKm
+	}
 }
