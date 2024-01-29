a3965f4a0a9b9a4cf1b07a7072da93357453921b
Rolando Toledo Fernandez <rtoledofernandez@globalprimex.com>
2024-01-29 18:59:13 +0100

    adding kubernetes configuration and deployment script

---
+++ scripts/deployment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+cd ./kubernetes
+
+if [ ! -z "$DEPLOY_DB_SERVICE" ]; then
+    kubectl apply -f mongodb-deployment.yaml
+fi
+
+if [ ! -z "$DEPLOY_REDIS_SERVICE" ]; then
+    kubectl apply -f redis-deployment.yaml
+fi
+
+if [ ! -z "$DEPLOY_IDENTITY_SERVICE" ]; then
+    kubectl apply -f identity-deployment.yaml
+fi
+
+if [ ! -z "$DEPLOY_ORDER_SERVICE" ]; then
+    kubectl apply -f order-deployment.yaml
+fi
+
+if [ ! -z "$DEPLOY_WALLET_SERVICE" ]; then
+    kubectl apply -f wallet-deployment.yaml
+fi
+
+if [ -z "$DEPLOY_ORDER_SERVICE" ] &&
+    [ -z "$DEPLOY_WALLET_SERVICE" ] &&
+    [ -z "$DEPLOY_AUTH_SERVICE" ]; then
+    kubectl apply -f mongodb-deployment.yaml
+    kubectl apply -f redis-deployment.yaml
+    kubectl apply -f order-deployment.yaml
+    kubectl apply -f wallet-deployment.yaml
+    kubectl apply -f identity-deployment.yaml
+fi
+
+kubectl apply -f gateway-deployment.yaml
+
+echo "Deployment completed"
